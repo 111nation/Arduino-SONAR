b0f05f0a3ed05037b65a0c5b06c8852ed823fa79
Tafara <tafara.k@outlook.com>
2024-10-18 18:26:29 +0200

    Created SONAR repository

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <string.h>
+
+const std::wstring MAIN_CLASS = L"MAIN_CLASS";
+
+int WINAPI wWinMain(HINSTANCE hApp, HINSTANCE hPrev, PWSTR arg, int window_mode) {
+	
+
+		
+}
