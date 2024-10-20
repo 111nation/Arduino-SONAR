1a8a8b58898e5ff60fae4855048c1aafaa710133
Tafara <tafara.k@outlook.com>
2024-10-20 20:04:35 +0200

    Title bar complete

--- dashboard.cpp
+++ dashboard.cpp
@@ -1,13 +1,23 @@
 #include "dashboard.h"
 
 Paint::~Paint() {
-	if (font != nullptr) {
+	if (font != NULL) {
 		delete font;
 	}
 
 	if (hbrush) {
 		DeleteObject(hbrush);
 		hbrush = NULL;
+	}
+
+	if (area) {
+		DeleteObject(area);
+		hbrush = NULL;
+	}
+
+	if (hpen) {
+		DeleteObject(hpen);
+		hpen = NULL;
 	}
 
 }
@@ -17,32 +27,67 @@ void Paint::RoundRect(int corner_radius) {
 }
 
 void Paint::RoundRect(int cornerx, int cornery) {
-	ApplyColor();
-
-	const int x = pos.left;
-	const int y = pos.top;
-	const int xend = pos.right;
-	const int yend = pos.bottom;
+	UpdateColor();
 	
 	::RoundRect(area, x, y, xend, yend, cornerx, cornery);
-
-	DeleteObject(hbrush);
 }
 
-void Paint::FillRect() {
-	ApplyColor();
+void Paint::Rectangle() {
+	UpdateColor();
 
-	::FillRect(area, &pos, hbrush);
-
-	DeleteObject(hbrush);
+	::Rectangle(area, x, y, xend, yend);
 }
 
 //====================
 // UTILITIES
 //====================
-void Paint::ApplyColor() {
+void Paint::UpdateColor() {
+	DeleteObject(hbrush);
+	DeleteObject(hpen);
+
+	// Background color
 	hbrush = CreateSolidBrush(color);
 	SelectObject(area, hbrush); // Colours 
+	
+	// border color
+	if (border.width == 0) {
+		hpen = CreatePen(PS_NULL, 0, DEF_TRANSPARENT);
+	} else {
+		hpen = CreatePen(border.style, border.width, border.color);
+	}
+	SelectObject(area, hpen);
 
+}
+
+void Paint::RECTtoPos(RECT rect) {
+	x = rect.left;
+	y = rect.top;
+	xend = rect.right;
+	yend = rect.bottom;
+}
+
+void Paint::Reset() {
+	x = 0;
+	y = 0;
+	xend = 0;
+	yend = 0;
+	
+	border.width = 0;
+	border.style = PS_SOLID;
+	border.color = DEF_TRANSPARENT;
+
+	color = 0;
+
+	corner.x = 0;
+	corner.y = 0;
+	Transparency(DEF_TRANSPARENT, 0); 
+}
+
+void Paint::Transparency(int color, BYTE alpha) {
+	SetLayeredWindowAttributes(hWnd, color, alpha, LWA_COLORKEY);
+}
+
+void Paint::Transparency() {
+	Transparency(DEF_TRANSPARENT, 0);
 }
 
--- dashboard.h
+++ dashboard.h
@@ -10,29 +10,35 @@
 
 
 #define WINDOW_WIDTH 1000  
-#define TITLE_BAR 30
+#define TITLE_BAR 20
 #define WINDOW_HEIGHT 500
 
 // Colors
-#define TRANSPARENT_COLOR RGB(254, 254, 254)
-#define BACKGROUND_COLOR 0x000000
-#define MID_COLOR 0x66666E
+#define DEF_TRANSPARENT RGB(254, 254, 254)
+#define BACKGROUND_COLOR RGB(30, 31, 34) 
+#define MID_COLOR RGB(43, 45, 49) 
 #define ACCENT_0 0xF4F4F6 // Lightest
 #define ACCENT_1 0xE6E6E9
 #define ACCENT_2 0x9999A1 // Darkest
-			  
+
 // =================
 //  PAINTING DATA
 // =================
 class Paint {
 	private:
-	void ApplyColor();
-
-	public:
-	HDC area=nullptr;
-	HBRUSH hbrush=nullptr;
+	void UpdateColor();
+	HBRUSH hbrush=NULL;
+	HPEN hpen=NULL;
 	
-	RECT pos;
+	public:
+	HDC area=NULL;
+	HWND hWnd=NULL;
+	
+	int x = 0;
+	int y = 0;
+	int xend = 0;
+	int yend = 0;
+
 	struct Corner {
 		int x = 0;
 		int y = 0;
@@ -41,13 +47,23 @@ class Paint {
 
 	COLORREF color = 0;
 
+	struct Border{
+		int width = 0;
+		int color = 0;
+		int style = PS_SOLID;
+	};
+	Border border;
+
 	~Paint();
+	void Reset();
+	void RECTtoPos(RECT rect);
 
 	//====PAINT UTILS====
-	void FillRect();
+	void Rectangle();
 	void RoundRect(int corner_radius);
 	void RoundRect(int cornerx, int cornery);
+	void Transparency(int color, BYTE alpha); 
+	void Transparency();
 
-
-	Font * font = nullptr;
+	Font * font = NULL;
 };
